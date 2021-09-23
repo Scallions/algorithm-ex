acf64c10909baf920a08c66aebe9992adf05f7a7
Scallions <844120806@qq.com>
2021-09-23 15:54:39 +0800

    update leetcode and preorder

---
+++ oj/leetcode/144.二叉树的前序遍历.cpp
@@ -0,0 +1,121 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=cpp
+ *
+ * [144] 二叉树的前序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (70.28%)
+ * Likes:    650
+ * Dislikes: 0
+ * Total Accepted:    409K
+ * Total Submissions: 581.8K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,2,3]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：root = [1,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root == nullptr) return vector<int>();
+        stack<TreeNode*> sk;
+        sk.push(root);
+        vector<int> ans;
+        while(!sk.empty()){
+            TreeNode* node = sk.top();sk.pop();
+            ans.push_back(node->val);
+            if(node->right) sk.push(node->right);
+            if(node->left) sk.push(node->left);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
