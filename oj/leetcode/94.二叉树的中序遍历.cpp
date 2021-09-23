0c1f7db14c22084259d64ce00ecdaa52a8534f58
Scallions <844120806@qq.com>
2021-09-23 18:24:48 +0800

    add inorder

---
+++ oj/leetcode/94.二叉树的中序遍历.cpp
@@ -0,0 +1,129 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (75.59%)
+ * Likes:    1105
+ * Dislikes: 0
+ * Total Accepted:    558.1K
+ * Total Submissions: 738.2K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：root = [1,2]
+ * 输出：[2,1]
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // easy 递归
+        // if(root==nullptr){
+        //     return vector<int>();
+        // }
+        // vector<int> r,l,res;
+        // if(root->right)r = inorderTraversal(root->right);
+        // if(root->left)l = inorderTraversal(root->left);
+        // res = l;
+        // res.push_back(root->val);
+        // res.insert(res.end(), r.begin(), r.end());
+        // return res;
+
+        // 迭代
+        if(root==nullptr){
+            return vector<int>();
+        }
+        stack<TreeNode*> s;
+        vector<int> res;
+        while(root!=nullptr || !s.empty()){
+            while(root!=nullptr){
+                s.push(root);
+                root = root->left;
+            }
+            root = s.top(); s.pop();
+            res.push_back(root->val);
+            root = root->right;
+        }
+        return res;
+        
+    }
+};
+// @lc code=end
+
