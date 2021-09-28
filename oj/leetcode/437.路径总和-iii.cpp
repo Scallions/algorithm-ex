c51629f73951f26284648aa3f7f4750bed0c0466
Scallions <844120806@qq.com>
2021-09-28 09:54:42 +0800

    add leetcode 437

---
+++ oj/leetcode/437.路径总和-iii.cpp
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=437 lang=cpp
+ *
+ * [437] 路径总和 III
+ *
+ * https://leetcode-cn.com/problems/path-sum-iii/description/
+ *
+ * algorithms
+ * Medium (56.67%)
+ * Likes:    1025
+ * Dislikes: 0
+ * Total Accepted:    104.5K
+ * Total Submissions: 183.5K
+ * Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
+ *
+ * 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+ * 
+ * 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+ * 输出：3
+ * 解释：和等于 8 的路径有 3 条，如图所示。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ * 输出：3
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 二叉树的节点个数的范围是 [0,1000]
+ * -10^9  
+ * -1000  
+ * 
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        count = 0;
+        dfs(root, sum);
+        return count;
+    }
+
+    void dfs(TreeNode* root, int sum){
+        if(root==nullptr) return;
+        count += beginSum(root, sum);
+        dfs(root->right,sum);
+        dfs(root->left,sum);
+    }
+
+    int beginSum(TreeNode* root, int sum){
+        if(root==nullptr) return 0;
+        int r = root->val == sum;
+        return r+beginSum(root->left, sum-root->val) + beginSum(root->right, sum-root->val);
+    }
+private:
+    int count;
+};
+// @lc code=end
+
