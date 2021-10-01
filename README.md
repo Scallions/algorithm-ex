4b24c45c5361a41c754cefccf3c1b3f3d2702bb9
Scallions_ <844120806@qq.com>
2021-10-01 20:59:46 +0800

    Merge branch 'add-algorithm-stone'

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author       : Scallions
  * @Date         : 2020-03-19 21:07:37
  * @LastEditors  : Scallions
- * @LastEditTime : 2020-03-19 21:09:27
+ * @LastEditTime : 2021-10-01 20:58:06
  * @FilePath     : /algorithm-ex/README.md
  * @Description  : 
  -->
@@ -21,3 +21,31 @@
 - algorithm
   - sort
   - ...
+
+### Leetcode路线图合集
+采用[acm-clan/algorithm-stone](https://github.com/acm-clan/algorithm-stone)的脚本生成
+#### Tree路线图  
+![](images/leetcode_tree.svg)
+#### 动态规划路线图
+![](images/leetcode_dp.svg)
+
+<!-- #### 并查集路线图
+![](images/leetcode_union_find.svg) -->
+
+#### 堆栈队列路线图
+![](images/leetcode_heap_stack_queue.svg)
+
+#### 链表路线图
+![](images/leetcode_linked_list.svg)
+
+#### 二分查找
+![](images/leetcode_binary_search.svg)
+
+#### 几何题路线图
+![](images/leetcode_geometry.svg)
+
+#### 小众题路线图
+![](images/leetcode_mini.svg)
+
+### Codeforces Div.3 Div.2路线图
+![](images/codeforces.svg)
---
+++ oj/leetcode/101.对称二叉树.cpp
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=cpp
+ *
+ * [101] 对称二叉树
+ *
+ * https://leetcode-cn.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (56.34%)
+ * Likes:    1553
+ * Dislikes: 0
+ * Total Accepted:    412.5K
+ * Total Submissions: 732.2K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * 
+ * 
+ * 
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ * 
+ * 
+ * 
+ * 
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你可以运用递归和迭代两种方法解决这个问题吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return dfs(root, root);
+    }
+    
+private:
+    bool dfs(TreeNode* root, TreeNode* root1){
+        if(root == nullptr && root1 == nullptr) return true;
+        if(root == nullptr || root1 == nullptr) return false;
+        if(root->val != root1->val) return false;
+        if(!dfs(root->left, root1->right)) return false;
+        if(!dfs(root->right, root1->left)) return false;
+        return true;
+    }
+};
+// @lc code=end
+
