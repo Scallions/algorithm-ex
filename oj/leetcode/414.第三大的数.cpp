0c2db67805b68611975a92869497b72e2888fbaa
Scallions <844120806@qq.com>
2021-10-07 21:50:14 +0800

    update leetcodes

---
+++ oj/leetcode/414.第三大的数.cpp
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=414 lang=cpp
+ *
+ * [414] 第三大的数
+ *
+ * https://leetcode-cn.com/problems/third-maximum-number/description/
+ *
+ * algorithms
+ * Easy (36.40%)
+ * Likes:    300
+ * Dislikes: 0
+ * Total Accepted:    84K
+ * Total Submissions: 214.8K
+ * Testcase Example:  '[3,2,1]'
+ *
+ * 给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：[3, 2, 1]
+ * 输出：1
+ * 解释：第三大的数是 1 。
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：[1, 2]
+ * 输出：2
+ * 解释：第三大的数不存在, 所以返回最大的数 2 。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：[2, 2, 3, 1]
+ * 输出：1
+ * 解释：注意，要求返回第三大的数，是指在所有不同数字中排第三大的数。
+ * 此例中存在两个值为 2 的数，它们都排第二。在所有不同数字中排第三大的数为 1 。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * -2^31 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能设计一个时间复杂度 O(n) 的解决方案吗？
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int> s;
+        for(auto n: nums){
+            s.insert(n);
+            if(s.size()>3){
+                s.erase(s.begin());
+            }
+        }
+        if(s.size()<3) return *s.rbegin();
+        return *s.begin();
+    }
+};
+// @lc code=end
+
