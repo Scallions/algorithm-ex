b0758a860d1e9fa0e554cf6ab73d5a9d2179d2ca
Scallions_ <844120806@qq.com>
2021-10-23 16:45:52 +0800

    fix leetcode heap stack queue (#5)

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import codeforces_view
 import luogu_view
 
 def main():
-    # leetcode_view.process()
-    # codeforces_view.process()
+    leetcode_view.process()
+    codeforces_view.process()
     luogu_view.process()
 
 if __name__ == "__main__":
