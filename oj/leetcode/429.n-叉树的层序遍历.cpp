c9f13bc115a5d010316f6525fde0b0d4e292f8c9
Scallions <844120806@qq.com>
2021-10-24 16:55:25 +0800

    add leet 103,107,116,429

---
+++ oj/leetcode/429.n-叉树的层序遍历.cpp
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=429 lang=cpp
+ *
+ * [429] N 叉树的层序遍历
+ *
+ * https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (69.86%)
+ * Likes:    178
+ * Dislikes: 0
+ * Total Accepted:    60.2K
+ * Total Submissions: 86K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * 给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）。
+ * 
+ * 树的序列化输入是用层序遍历，每组子节点都由 null 值分隔（参见示例）。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [1,null,3,2,4,null,5,6]
+ * 输出：[[1],[3,2,4],[5,6]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root =
+ * [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+ * 输出：[[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树的高度不会超过 1000
+ * 树的节点总数在 [0, 10^4] 之间
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        if(root == nullptr) return {};
+        queue<Node*> q;
+        q.push(root);
+        vector<vector<int>> res;
+        int ql;
+        while(!q.empty()){
+            vector<int> tmp;
+            ql = q.size();
+            for(int i=0; i<ql; ++i){
+                root = q.front(); q.pop();
+                tmp.push_back(root->val);
+                for(auto c: root->children){
+                    q.push(c);
+                }
+            }
+            res.push_back(tmp);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
