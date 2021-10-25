2a066a635e77eb3c8ec9199160c5a83d0d754f10
Scallions_ <844120806@qq.com>
2021-10-25 08:15:51 +0000

    add leet 513,515,623

---
+++ oj/leetcode/513.找树左下角的值.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=513 lang=cpp
+ *
+ * [513] 找树左下角的值
+ *
+ * https://leetcode-cn.com/problems/find-bottom-left-tree-value/description/
+ *
+ * algorithms
+ * Medium (73.12%)
+ * Likes:    214
+ * Dislikes: 0
+ * Total Accepted:    52.7K
+ * Total Submissions: 72K
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给定一个二叉树的 根节点 root，请找出该二叉树的 最底层 最左边 节点的值。
+ * 
+ * 假设二叉树中至少有一个节点。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 
+ * 
+ * 输入: root = [2,1,3]
+ * 输出: 1
+ * 
+ * 
+ * 示例 2:
+ * 
+ * ⁠
+ * 
+ * 
+ * 输入: [1,2,3,4,null,5,6,null,null,7]
+ * 输出: 7
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 二叉树的节点个数的范围是 [1,10^4]
+ * -2^31  
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        if(root == nullptr) return -1;
+        int l;
+        queue<TreeNode*> q;
+        int ql;
+        q.push(root);
+        while(!q.empty()){
+            ql = q.size();
+            l = q.front()->val;
+            for(int i=0;i<ql;++i){
+                root = q.front();
+                q.pop();
+                if(root->left) q.push(root->left);
+                if(root->right) q.push(root->right);
+            }
+        }
+        return l;
+    }
+};
+// @lc code=end
+
