bd5f69a33e8344c2cbb732eebdc005a2f08af30f
Scallions_ <844120806@qq.com>
2021-10-29 14:10:59 +0000

    add leet 198,213,337,509,746

---
+++ oj/leetcode/213.打家劫舍-ii.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=213 lang=cpp
+ *
+ * [213] 打家劫舍 II
+ *
+ * https://leetcode-cn.com/problems/house-robber-ii/description/
+ *
+ * algorithms
+ * Medium (43.33%)
+ * Likes:    809
+ * Dislikes: 0
+ * Total Accepted:    173.9K
+ * Total Submissions: 401.2K
+ * Testcase Example:  '[2,3,2]'
+ *
+ * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈
+ * ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+ * 
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [2,3,2]
+ * 输出：3
+ * 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [1,2,3,1]
+ * 输出：4
+ * 解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+ * 偷窃到的最高金额 = 1 + 3 = 4 。
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [0]
+ * 输出：0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+#include <bits/stdc++.h>
+#include "struct.h"
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int a = nums[0], b = 0;
+        int t;
+        for(int i=1; i<n-1; ++i){
+            t = a;
+            a = max(a, b+nums[i]);
+            b = max(t, b);
+        }
+        int t1;
+        t1 = max(a, b);
+        a = 0;
+        b = 0;
+        for(int i=1; i<n; ++i){
+            t = a;
+            a = max(a, b+nums[i]);
+            b = max(t, b);
+        }
+        t = max(a, b);
+        return max(t, t1);
+    }
+};
+// @lc code=end
+
