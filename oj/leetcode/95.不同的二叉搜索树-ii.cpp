126490f5800540aaf523ecb5a906d504fc06dc3d
Scallions_ <844120806@qq.com>
2021-11-09 02:35:01 +0000

    add leet 95,105,108

---
+++ oj/leetcode/95.不同的二叉搜索树-ii.cpp
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=95 lang=cpp
+ *
+ * [95] 不同的二叉搜索树 II
+ *
+ * https://leetcode-cn.com/problems/unique-binary-search-trees-ii/description/
+ *
+ * algorithms
+ * Medium (70.29%)
+ * Likes:    1035
+ * Dislikes: 0
+ * Total Accepted:    104.6K
+ * Total Submissions: 148.6K
+ * Testcase Example:  '3'
+ *
+ * 给你一个整数 n ，请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。可以按 任意顺序 返回答案。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 3
+ * 输出：[[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 1
+ * 输出：[[1]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 
+ * 
+ * 
+ * 
+ */
+#include <bits/stdc++.h>
+#include "struct.h"
+using namespace std;
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        return generateTrees(1, n);
+    }
+private:
+    vector<TreeNode*> generateTrees(int s, int e){
+        // 创建以s开始e结束的二叉搜索树
+        if(s > e){
+            return {};
+        }
+        vector<TreeNode*> res;
+        for(int i=s; i<=e; ++i){
+            vector<TreeNode*> lefts = generateTrees(s, i-1);
+            vector<TreeNode*> rights = generateTrees(i+1, e);
+            if(lefts.size() == 0) lefts.push_back(nullptr);
+            if(rights.size() == 0) rights.push_back(nullptr);
+            for(auto left: lefts){
+                for(auto right: rights){
+                    TreeNode* root = new TreeNode(i);
+                    root->left = left;
+                    root->right = right;
+                    res.push_back(root);
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
