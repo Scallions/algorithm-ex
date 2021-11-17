c0f49393a77d6917b753b003d52c4a040e3d17be
Scallions_ <844120806@qq.com>
2021-11-17 03:37:05 +0000

    add leet 318,1123,1315

---
+++ oj/leetcode/318.最大单词长度乘积.cpp
@@ -0,0 +1,119 @@
+/*
+ * @lc app=leetcode.cn id=318 lang=cpp
+ *
+ * [318] 最大单词长度乘积
+ *
+ * https://leetcode-cn.com/problems/maximum-product-of-word-lengths/description/
+ *
+ * algorithms
+ * Medium (68.86%)
+ * Likes:    216
+ * Dislikes: 0
+ * Total Accepted:    25K
+ * Total Submissions: 35.4K
+ * Testcase Example:  '["abcw","baz","foo","bar","xtfn","abcdef"]'
+ *
+ * 给定一个字符串数组 words，找到 length(word[i]) * length(word[j])
+ * 的最大值，并且这两个单词不含有公共字母。你可以认为每个单词只包含小写字母。如果不存在这样的两个单词，返回 0。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: ["abcw","baz","foo","bar","xtfn","abcdef"]
+ * 输出: 16 
+ * 解释: 这两个单词为 "abcw", "xtfn"。
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: ["a","ab","abc","d","cd","bcd","abcd"]
+ * 输出: 4 
+ * 解释: 这两个单词为 "ab", "cd"。
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入: ["a","aa","aaa","aaaa"]
+ * 输出: 0 
+ * 解释: 不存在这样的两个单词。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 2 
+ * 1 
+ * words[i] 仅包含小写字母
+ * 
+ * 
+ */
+#include <bits/stdc++.h>
+#include "struct.h"
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        // sort(words.begin(), words.end(), [](auto a, auto b){
+        //     return a.size() > b.size();
+        // });
+        vector<int> cache;
+        int n = words.size();
+        int res = 0;
+        for(int i=0; i<n; ++i){
+            string word = words[i];
+            int t = 0;
+            for(auto c: word){
+                t |= 1 << (c-'a');
+            }
+            cache.push_back(t);
+            for(int j=0; j<i; ++j){
+                if((cache[j] & t) == 0){
+                    res = max(res, int(words[j].size() * words[i].size()));
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
+// 官方题解，进一步去除了同样🐴的单词的一些重复运算
+class Solution1 {
+public:
+    int maxProduct(vector<string>& words) {
+        unordered_map<int,int> map;
+        int length = words.size();
+        for (int i = 0; i < length; i++) {
+            int mask = 0;
+            string word = words[i];
+            int wordLength = word.size();
+            for (int j = 0; j < wordLength; j++) {
+                mask |= 1 << (word[j] - 'a');
+            }
+            if(map.count(mask)) {
+                if (wordLength > map[mask]) {
+                    map[mask] = wordLength;
+                }
+            } else {
+                map[mask] = wordLength;
+            }
+            
+        }
+        int maxProd = 0;
+        for (auto [mask1, _] : map) {
+            int wordLength1 = map[mask1];
+            for (auto [mask2, _] : map) {
+                if ((mask1 & mask2) == 0) {
+                    int wordLength2 = map[mask2];
+                    maxProd = max(maxProd, wordLength1 * wordLength2);
+                }
+            }
+        }
+        return maxProd;
+    }
+};
