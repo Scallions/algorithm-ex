383fa0e48693d23f563aae90d3919df4d277a8ab
Scallions_ <844120806@qq.com>
2021-11-26 01:59:16 +0000

    add leet 42,139,700

---
+++ oj/leetcode/42.接雨水.cpp
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=cpp
+ *
+ * [42] 接雨水
+ *
+ * https://leetcode-cn.com/problems/trapping-rain-water/description/
+ *
+ * algorithms
+ * Hard (58.66%)
+ * Likes:    2887
+ * Dislikes: 0
+ * Total Accepted:    356.3K
+ * Total Submissions: 606.1K
+ * Testcase Example:  '[0,1,0,2,1,0,1,3,2,1,2,1]'
+ *
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：height = [4,2,0,3,2,5]
+ * 输出：9
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * n == height.length
+ * 1 <= n <= 2 * 10^4
+ * 0 <= height[i] <= 10^5
+ * 
+ * 
+ */
+#include <bits/stdc++.h>
+#include "struct.h"
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0;
+        int r = height.size()-1;
+        int lm = height[l], rm = height[r];
+        int res=0;
+        while(l<r){
+            if(lm<rm){
+                res += lm-height[l];
+                ++l;
+                lm = max(lm, height[l]);
+            }else{
+                res += rm-height[r];
+                --r;
+                rm = max(rm, height[r]);
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
