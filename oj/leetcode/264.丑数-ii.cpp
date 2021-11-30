3aa15a1807d99dadf4f86657345b43b60d01d438
Scallions_ <844120806@qq.com>
2021-11-30 08:53:58 +0000

    add leets

---
+++ oj/leetcode/264.丑数-ii.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=264 lang=cpp
+ *
+ * [264] 丑数 II
+ *
+ * https://leetcode-cn.com/problems/ugly-number-ii/description/
+ *
+ * algorithms
+ * Medium (58.04%)
+ * Likes:    786
+ * Dislikes: 0
+ * Total Accepted:    105.3K
+ * Total Submissions: 181.6K
+ * Testcase Example:  '10'
+ *
+ * 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+ * 
+ * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 10
+ * 输出：12
+ * 解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 1
+ * 输出：1
+ * 解释：1 通常被视为丑数。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 
+ * 
+ */
+#include <bits/stdc++.h>
+#include "struct.h"
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> dp;
+        dp.push_back(1);
+        vector<int> idx(3,0);
+        vector<int> dd{2,3,5};
+        vector<int> mm{2,3,5};
+        for(int i=1; i<n; ++i){
+            int m=INT_MAX;
+            for(int j=0;j<3;++j){
+                if(mm[j]<m){
+                    m = mm[j];
+                }
+            }
+            dp.push_back(m);
+            for(int j=0;j<3;++j){
+                if(mm[j] == m){
+                    ++idx[j];
+                    mm[j] = dp[idx[j]] * dd[j];
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
+// @lc code=end
+
---
+++ oj/leetcode/96.不同的二叉搜索树.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=96 lang=cpp
+ *
+ * [96] 不同的二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/unique-binary-search-trees/description/
+ *
+ * algorithms
+ * Medium (69.97%)
+ * Likes:    1426
+ * Dislikes: 0
+ * Total Accepted:    178.2K
+ * Total Submissions: 254.8K
+ * Testcase Example:  '3'
+ *
+ * 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 3
+ * 输出：5
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 1
+ * 输出：1
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 
+ * 
+ */
+#include <bits/stdc++.h>
+#include "struct.h"
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1,1);
+        for(int i=2; i<=n; ++i){
+            int res = 0;
+            for(int j=0; j<i;++j){
+                res += dp[j] * dp[i-1-j];
+            }
+            dp[i] = res;
+        }
+        return dp[n];
+    }
+};
+// @lc code=end
+
+class Solution1 {
+public:
+    int numTrees(int n) {
+        vector<int> res{1
+            ,2
+            ,5
+            ,14
+            ,42
+            ,132
+            ,429
+            ,1430
+            ,4862
+            ,16796
+            ,58786
+            ,208012
+            ,742900
+            ,2674440
+            ,9694845
+            ,35357670
+            ,129644790
+            ,477638700
+            ,1767263190};
+        return res[n-1];
+    }
+};
