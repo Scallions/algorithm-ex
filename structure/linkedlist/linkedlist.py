17b4e8024bbc66a65d4802124cd432808789ffd0
Scallions_ <844120806@qq.com>
2023-06-23 08:35:11 +0000

    update 单向链表

--- structure/linkedlist/linkedlist.py
+++ structure/linkedlist/linkedlist.py
@@ -4,7 +4,7 @@
 @LastEditors  : Scallions
 @LastEditTime : 2020-03-20 11:19:04
 @FilePath     : /algorithm-ex/structure/linkedlist/linkedlist.py
-@Description  : 
+@Description  : 单向链表
 '''
 
 
@@ -18,6 +18,23 @@ class Node:
         return super().__repr__()
         return f"Node: {str(self.data)}"
 
+    def insert(self, v):
+        next_node = Node(v)
+        self.next = next_node
+        return next_node
+
+    def delete(self):
+        if self.next:
+            self.data = self.next.data
+            self.next = self.next.next
+            return self
+        
+
 
 if __name__ == "__main__":
-    pass
+    node = Node(1)
+    node.insert(2).insert(3).insert(4)
+    while node:
+        print(node.data)
+        node = node.next
+    
