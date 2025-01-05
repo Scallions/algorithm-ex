b8619277ff3d6bbf7fcd8ee0911c2374c21590be
Scallions_ <844120806@qq.com>
2025-01-05 01:45:26 +0000

    leet: 1366

---
+++ oj/leetcode/1366.通过投票对团队排名.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=1366 lang=python3
+#
+# [1366] 通过投票对团队排名
+#
+
+# @lc code=start
+class Solution:
+    def rankTeams(self, votes: List[str]) -> str:
+        
+# @lc code=end
+
